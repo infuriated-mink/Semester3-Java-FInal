158e12954b78fe7d294c74f179a07d4cb490faef
Vanessa Rice <vanessa.rice@keyin.com>
2024-04-08 11:54:20 -0230

    removed .jar from gitignore.

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String url = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String url = "jdbc:postgresql://localhost:5433/postgres";
     private static final String user = "vanessa";
     private static final String password = "Q123w123!";
 
